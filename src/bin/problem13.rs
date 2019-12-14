b14b2d8706917d3cbbe710805cc6ddd1b58c1769
simpuid <utkarsh.email@yahoo.com>
2019-12-14 19:26:01 +0530

    removed ggez dependency

--- src/bin/problem13.rs
+++ src/bin/problem13.rs
@@ -1,11 +1,5 @@
 extern crate aoc;
 use aoc::int_code::IntProgram;
-use ggez::event::{KeyCode, KeyMods};
-use ggez::graphics::{Color, DrawMode, Mesh, Rect};
-use ggez::*;
-use ggez::{event, graphics};
-
-const SIZE: f32 = 20f32;
 
 fn block_count(v: &[i64]) -> isize {
     let mut iter = v.iter();
@@ -18,97 +12,13 @@ fn block_count(v: &[i64]) -> isize {
     result
 }
 
-struct MainState {
-    program: IntProgram,
-    left: bool,
-    right: bool,
-    block_code: Vec<i64>,
-    delta: f64,
-    paint: bool,
-    paddle: i64,
-    ball: i64,
-}
-
-impl MainState {
-    fn new(code: Vec<i64>) -> MainState {
-        MainState {
-            program: IntProgram::new(code),
-            left: false,
-            right: false,
-            block_code: Vec::new(),
-            delta: 0f64,
-            paint: false,
-            paddle: 0,
-            ball: 0,
-        }
-    }
-}
-
-impl ggez::event::EventHandler for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        if !self.paint {
-            self.delta -= 0.002f64;
-            self.block_code = self.program.execute(vec![aoc::sign((self.ball - self.paddle) as i32) as i64]);
-            self.paint = true;
-        }
-        ggez::timer::yield_now();
-        Ok(())
-    }
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        if self.paint {
-            self.paint = false;
-            let empty = Mesh::new_rectangle(ctx, DrawMode::fill(), Rect::new(0f32, 0f32, SIZE, SIZE), Color::new(0f32, 0f32, 0f32, 1f32)).unwrap();
-            let wall = Mesh::new_rectangle(ctx, DrawMode::fill(), Rect::new(0f32, 0f32, SIZE, SIZE), Color::new(0.2f32, 0.2f32, 0.2f32, 1f32)).unwrap();
-            let block = Mesh::new_rectangle(ctx, DrawMode::fill(), Rect::new(0f32, 0f32, SIZE, SIZE), Color::new(1f32, 1f32, 1f32, 1f32)).unwrap();
-            let paddle = Mesh::new_rectangle(ctx, DrawMode::fill(), Rect::new(0f32, 0f32, SIZE, SIZE), Color::new(0.2f32, 0.2f32, 1f32, 1.2f32)).unwrap();
-            let ball = Mesh::new_rectangle(ctx, DrawMode::fill(), Rect::new(0f32, 0f32, SIZE, SIZE), Color::new(0.2f32, 0.9f32, 0.2f32, 1.2f32)).unwrap();
-            let mut iter = self.block_code.iter();
-            while let (Some(x), Some(y), Some(id)) = (iter.next(), iter.next(), iter.next()) {
-                if let (-1, 0, score) = (x, y, id) {
-                    println!("new score {}", score);
-                } else {
-                    let rec = match id {
-                        1 => &wall,
-                        2 => &block,
-                        3 => {
-                            self.paddle = *x;
-                            &paddle
-                        }
-                        4 => {
-                            self.ball = *x;
-                            &ball
-                        }
-                        _ => &empty,
-                    };
-                    graphics::draw(
-                        ctx,
-                        rec,
-                        (ggez::mint::Point2 {
-                            x: *x as f32 * 10f32 + 100f32,
-                            y: *y as f32 * 10f32 + 100f32,
-                        },),
-                    )?;
-                }
-            }
-            graphics::present(ctx)?;
-        }
-        ggez::timer::yield_now();
-        Ok(())
-    }
-    fn key_down_event(&mut self, _ctx: &mut Context, key_code: KeyCode, _keymods: KeyMods, _repeat: bool) {
-        match key_code {
-            KeyCode::Left => self.left = true,
-            KeyCode::Right => self.right = true,
-            _ => (),
-        }
-    }
-
-    fn key_up_event(&mut self, _ctx: &mut Context, key_code: KeyCode, _keymods: KeyMods) {
-        match key_code {
-            KeyCode::Left => self.left = false,
-            KeyCode::Right => self.right = false,
-            _ => (),
-        }
+fn sign(a: i64) -> i64 {
+    if a > 0 {
+        1
+    } else if a < 0 {
+        -1
+    } else {
+        0
     }
 }
 
@@ -117,9 +27,30 @@ fn main() {
     let mut comp = IntProgram::new(code.clone());
     let output = comp.execute(Vec::new());
     println!("block count: {}", block_count(&output));
-    let cb = ggez::ContextBuilder::new("int code arcade", "aoc author");
-    let (ctx, event_loop) = &mut cb.build().unwrap();
-    let state = &mut MainState::new(code);
-    state.program.set(0, 2);
-    event::run(ctx, event_loop, state).unwrap();
+    let mut program = IntProgram::new(code);
+    program.set(0, 2);
+    let mut ball_pos: i64 = 0;
+    let mut paddle_pos: i64 = 0;
+    let mut score: i64 = -1;
+    print!("playing");
+    let mut dot_counter = 0;
+    while program.can_run() {
+        let output = program.execute(vec![sign(ball_pos - paddle_pos)]);
+        let mut iter = output.into_iter();
+        while let (Some(x), Some(y), Some(id)) = (iter.next(), iter.next(), iter.next()) {
+            match (x, y, id) {
+                (-1, 0, new_score) => score = new_score,
+                (x, _, 3) => paddle_pos = x,
+                (x, _, 4) => ball_pos = x,
+                _ => (),
+            }
+        }
+        dot_counter += 1;
+        while dot_counter > 100 {
+            dot_counter -= 100;
+            print!(".");
+        }
+    }
+    println!("finished");
+    println!("final score: {}", score);
 }
